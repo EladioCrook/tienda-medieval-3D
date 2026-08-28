2e5800244f394b710fc04cfa765414b3b4079d00
Eladio Martínez <eladiomartinez3d@gmail.com>
2026-08-28 09:39:19 +0200

    primera taza

--- src/main.js
+++ src/main.js
@@ -8,20 +8,22 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+camera.position.y= 3;
 const renderer = new THREE.WebGLRenderer({canvas:document.querySelector("#visor3d")});
 renderer.setSize(window.innerWidth, window.innerHeight);
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 
 // Geometría
-/*<--Eliminar esta linea
 const loader = new GLTFLoader();
 
-loader.load('/src/assets/nombre-del-modelo-1.glb', (gltf) => {
+loader.load('/src/assets/taza1.glb', (gltf) => {
   const modelo = gltf.scene;
   scene.add(modelo);
-});
-
+  modelo.scale.set(20, 20, 20)
+  });
+  
+  /*<--Eliminar esta linea
 loader.load('/src/assets/nombre-del-modelo-2.glb', (gltf) => {
   const modelo = gltf.scene;
   scene.add(modelo);
