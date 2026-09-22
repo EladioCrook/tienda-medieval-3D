f185c6efe0267c61834fb5a7651fed7f88553e3c
Eladio Martínez <eladiomartinez3d@gmail.com>
2026-09-22 08:25:27 +0200

    datos y descripción de las tazas rellenados

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Escena
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x8d8d8d);
 const visor3d = document.querySelector("#visor3d");
 const camera = new THREE.PerspectiveCamera(75, visor3d.clientWidth / visor3d.clientHeight, 0.1, 1000);
 camera.position.z = 5;
@@ -53,6 +54,17 @@ function ajustarTamano() {
 renderer.setAnimationLoop(render);
 
 
+// Datos
+const datosTazas = {
+  1: { nombre: "Taza de Almagre", descripcion: "Taza de tamaño pequeño muy elegante, decorada con un pigmento rojizo obtenido a partir del óxido de hierro.", precio: "2 Monedas de Oro" },
+  2: { nombre: "Taza de Oropimente", descripcion: "Taza de tamaño medio muy elegante, decorada con un mineral amarillo intenso usado como pigmento.", precio: "3 Monedas de Oro" },
+  3: { nombre: "Taza de Añil", descripcion: "Taza de tamaño grande muy elegante, decorada con un tinte azul obtenido a partir de la planta Añil.", precio: "4 Monedas de Oro" }
+}
+
+const nombre = document.querySelector("#nombre-producto");
+const descripcion = document.querySelector("#texto-producto");
+
+
 // Numeración de la galería
 const tarjetas = document.querySelectorAll(".tarjeta-producto")
 
@@ -72,5 +84,8 @@ tarjetas.forEach((tarjeta) => {
     vistaProducto.style.display = "flex"
 
     ajustarTamano()
+
+    nombre.textContent = datosTazas[numero].nombre
+    descripcion.textContent = datosTazas[numero].descripcion
   })
 });
