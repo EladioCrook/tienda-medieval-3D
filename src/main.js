baff013a8df528650138025174de0eacfd3b7002
Eladio Martínez <eladiomartinez3d@gmail.com>
2026-09-23 08:09:27 +0200

    encuadre automático de cámara con Box3, tazas de distinto tamaño bien centradas

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x8d8d8d);
 const visor3d = document.querySelector("#visor3d");
 const camera = new THREE.PerspectiveCamera(75, visor3d.clientWidth / visor3d.clientHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 3;
 camera.position.y= 3;
 const renderer = new THREE.WebGLRenderer({canvas:document.querySelector("#visor3d")});
 const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -29,6 +29,19 @@ function cargarTaza(numero) {
     scene.add(modelo);
     modeloActual = modelo
     modelo.scale.set(20, 20, 20)
+    const caja = new THREE.Box3().setFromObject(modelo);
+    const tamano = caja.getSize(new THREE.Vector3());
+    const centro = caja.getCenter(new THREE.Vector3());
+    const dimensionMaxima = Math.max(tamano.x, tamano.y, tamano.z);
+    const distancia = dimensionMaxima / (2 * Math.tan((Math.PI * camera.fov) / 360));
+    camera.position.set(
+    centro.x,
+    centro.y + distancia * 0.5,
+    centro.z + distancia * 2.2
+    );
+    camera.lookAt(centro);
+    orbitControls.target.copy(centro);
+    orbitControls.update();
   });
 }
 
@@ -82,7 +95,7 @@ tarjetas.forEach((tarjeta) => {
     presentacion.style.display = "none"
     galeria.style.display = "none"
     visor3d.style.display = "block"
-    vistaProducto.style.display = "flex"
+    vistaProducto.style.display = "block"
 
     ajustarTamano()
 
